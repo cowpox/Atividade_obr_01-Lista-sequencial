f1682501f6406feaa5912c94b15fe8fa6ed76c52
Adriano Lucio Uchoa Brandao <adrianolucio@gmail.com>
2024-11-25 01:21:04 -0300

    Terceiro teste (atualizar os arquivos restantes)

--- Dinamica/ClienteOrdenadaDin teste.c
+++ Dinamica/ClienteOrdenadaDin teste.c
@@ -1,75 +1,51 @@
 // main.c
 #include "ListaOrdenadaDin.c"
 
-
 int main() {
-  LISTA lista;
-  REGISTRO reg;
+    LISTA lista;
+    REGISTRO reg;
 
-  // Inicializar a lista
-  inicializarLista(&lista);
+    // Inicializar a lista
+    inicializarLista(&lista);
 
-  // Exibir lista vazia
-  exibirLista(&lista);
-  printf("Numero de elementos na lista: %i.\n", tamanho(&lista));
-  printf("Tamanho da lista (em bytes): %i.\n", tamanhoEmBytes(&lista));
-  printf("Tamanho da lista em elementos: %d.\n", tamanhoEmElementos(&lista));
-  printf("Calcula tamanho em bytes: %i.\n", calculaTamanhoEmBytes(tamanhoEmElementos(&lista)));
+    // Exibir lista vazia
+    exibirListaDetalhada(&lista);
 
-  // Inserir elementos na lista
-  reg.chave = 9;
-  inserirElemListaOrd(&lista, reg);
+    // Inserir elementos na lista
+    reg.chave = 9;
+    inserirElemListaOrd(&lista, reg);
 
-
-
-  reg.chave = 3;
-  inserirElemListaOrd(&lista, reg);
+    reg.chave = 3;
+    inserirElemListaOrd(&lista, reg);
 
     // Exibir lista após inserções
-  exibirLista(&lista);
-  printf("Numero de elementos na lista: %i.\n", tamanho(&lista));
-  printf("Tamanho da lista (em bytes): %i.\n", tamanhoEmBytes(&lista));
-  printf("Tamanho da lista em elementos: %d.\n", tamanhoEmElementos(&lista));
-  printf("Calcula tamanho em bytes: %i.\n", calculaTamanhoEmBytes(tamanhoEmElementos(&lista)));
+    exibirListaDetalhada(&lista);
 
-  reg.chave = 4;
-  inserirElemListaOrd(&lista, reg);
+    reg.chave = 4;
+    inserirElemListaOrd(&lista, reg);
 
     // Exibir lista após inserções
-  exibirLista(&lista);
-  printf("Numero de elementos na lista: %i.\n", tamanho(&lista));
-  printf("Tamanho da lista (em bytes): %i.\n", tamanhoEmBytes(&lista));
-  printf("Tamanho da lista em elementos: %d.\n", tamanhoEmElementos(&lista));
-  printf("Calcula tamanho em bytes: %i.\n", calculaTamanhoEmBytes(tamanhoEmElementos(&lista)));
+    exibirListaDetalhada(&lista);
 
+    reg.chave = 1;
+    inserirElemListaOrd(&lista, reg);
+    reg.chave = 12;
+    inserirElemListaOrd(&lista, reg);
 
-  reg.chave = 1;
-  inserirElemListaOrd(&lista, reg);
-  reg.chave = 12;
-  inserirElemListaOrd(&lista, reg);
+    // Exibir lista após inserções
+    exibirListaDetalhada(&lista);
 
-  // Exibir lista após inserções
-  exibirLista(&lista);
-  printf("Numero de elementos na lista: %i.\n", tamanho(&lista));
-  printf("Tamanho da lista (em bytes): %i.\n", tamanhoEmBytes(&lista));
-  printf("Tamanho da lista em elementos: %d.\n", tamanhoEmElementos(&lista));
-  printf("Calcula tamanho em bytes: %i.\n", calculaTamanhoEmBytes(tamanhoEmElementos(&lista)));
-
-  reg.chave = 15;
-  inserirElemListaOrd(&lista, reg);
+    reg.chave = 15;
+    inserirElemListaOrd(&lista, reg);
     reg.chave = 18;
-  inserirElemListaOrd(&lista, reg);
+    inserirElemListaOrd(&lista, reg);
     reg.chave = 23;
-  inserirElemListaOrd(&lista, reg);
+    inserirElemListaOrd(&lista, reg);
     reg.chave = 25;
-  inserirElemListaOrd(&lista, reg);
+    inserirElemListaOrd(&lista, reg);
 
-   // Exibir lista após inserções
-  exibirLista(&lista);
-  printf("Numero de elementos na lista: %i.\n", tamanho(&lista));
-  printf("Tamanho da lista (em bytes): %i.\n", tamanhoEmBytes(&lista)); 
-  printf("Tamanho da lista em elementos: %d.\n", tamanhoEmElementos(&lista));
-  printf("Calcula tamanho em bytes: %i.\n", calculaTamanhoEmBytes(tamanhoEmElementos(&lista)));
+    // Exibir lista após inserções
+    exibirListaDetalhada(&lista);
 
-  return 0;
+    return 0;
 }
